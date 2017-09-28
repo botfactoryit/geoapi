abab9008972b3872a5ce5001eb785eb56bb56ebd
Matteo <m.contrini@gmail.com>
2017-09-28 18:54:38 +0200

    scaffolding

---
+++ index.js
@@ -0,0 +1,18 @@
+// Initialize the logger
+const bole = require('bole');
+
+bole.output([{ level: 'debug', stream: process.stdout }]);
+
+const logger = bole('index');
+logger.info('geoapi is booting...');
+
+// When an expection occurs,
+// log the 'Error' and euthanasia
+process.on('uncaughtException', (err) => {
+	logger.error(err);
+	// We can safely exit because the only logger output is stdout,
+	// which is flushed automatically when the process shuts down
+	process.exit(1);
+});
+
+require('./lib/server.js');
